1d02042219e695ba4d4413c686ccac784c567f39
barbara-pronsato <barbara.pronsato@gmail.com>
2020-07-23 19:19:26 -0700

    add files assignment 3

--- data.js
+++ data.js
@@ -1,11 +1,20 @@
 let art =  [
-{type: "sculpture", price: 1000, media: "bronze", artist_name: "Tilden_Swayze"}, 
-{type: "painting", price: 1000, media: "acrylic_canvas", artist_name: "Savannah_Georgia"},
-{type: "photography", price: 1000, media: "gelatin_silver", artist_name: "Eric_Fromm"},
-{type: "collage", price: 1000, media: "multi_media", artist_name: "Brian_Ferry"},
-{type: "illustration", price: 1000, media: "ink_paper", artist_name: "Solange_Cherry"}
+{title: "Sunset", type: "sculpture", price: 5000, media: "Bronze", artist_name: "Tilden Swayze", id: 0}, 
+{title: "Burst", type: "painting", price: 900, media: "Acrylic Canvas", artist_name: "Savannah Georgia", id: 1},
+{title: "Walk", type: "photography", price: 100, media: "Gelatin Silver", artist_name: "Eric Fromm", id: 2},
+{title: "Bridge Too Far", type: "collage", price: 100, media: "Multi Media", artist_name: "Brian Ferry", id: 3},
+{title: "Yellow Scream", type: "illustration", price: 300, media: "Ink Paper", artist_name: "Solange Cherry", id: 4}
 ];
 
 exports.getAll = () => {
     return art;
+};
+
+exports.getDetail = id => {
+    const artItem = art.find(art => art.id === id);
+    if (artItem === undefined) {
+        return {"details": false, "msg": `Artwork with ID "${id}" not found`}
+    } else {
+    return artItem;
+    }
 };
--- index.js
+++ index.js
@@ -1,26 +1,74 @@
 
 const http = require("http");
 const art = require('./data');
+const express = require("express");
+const bodyParser = require("body-parser")
+
+const app = express();
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({
+  defaultLayout: false
+}));
+
+app.set('view engine', 'handlebars');
 
 const displayArt = art.getAll();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public')); // set location for static files
+app.use(bodyParser.urlencoded({extended: true})); // parse form submission
+
+// Route handlers app.get()  app.post() error handlers app.use():
+
+// send static file as response
+app.get('/', (req, res) => {
+  res.type('text/html');
+  res.render('home', {art: displayArt});
+ });
  
-http.createServer((req,res) => {
-  const path = req.url.toLowerCase();
-  switch(path) {
-    case '/':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end("There are " + displayArt.length + " pieces of art for  my art pop-up: ");
-      break;
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About page:  Here is a little bit about me, I was born in Argentina, I have lived in Seattle for about 24 years and I am a designer');
-      break;
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Not found');
-      break;
-    }
-}).listen(process.env.PORT || 3000);
+ // send plain text response
+ app.get('/about', (req, res) => {
+  res.type('text/plain');
+  res.send('About page');
+ });
+
+ // send dynamic page response
+ app.get('/detail', (req, res) => {
+  const id = parseInt(req.query.id);
+  const details = art.getDetail(id);
+  res.render('detail', {id: id, details: details});
+});
+
+  // define 404 handler
+app.use( (req,res) => {
+  res.type('text/plain'); 
+  res.status(404);
+  res.send('404 - Not found');
+ });
+
+ // Once defined, the web server can be started:
+ app.listen(app.get('port'), () => {
+  console.log('Express started'); 
+ });
+ 
+// http.createServer((req,res) => {
+//   const path = req.url.toLowerCase();
+//   switch(path) {
+//     case '/':
+//       res.writeHead(200, {'Content-Type': 'text/plain'});
+//       res.end("There are " + displayArt.length + " pieces of art for  my art pop-up: ");
+//       break;
+//     case '/about':
+//       res.writeHead(200, {'Content-Type': 'text/plain'});
+//       res.end('About page:  Here is a little bit about me, I was born in Argentina, I have lived in Seattle for about 24 years and I am a designer');
+//       break;
+//     default:
+//       res.writeHead(404, {'Content-Type': 'text/plain'});
+//       res.end('Not found');
+//       break;
+//     }
+// }).listen(process.env.PORT || 3000);
 
 // Console will print the message
 console.log('Server running at http://127.0.0.1:3000/');
